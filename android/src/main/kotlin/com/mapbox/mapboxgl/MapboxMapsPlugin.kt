1b1ca7accb46b783fc1fbecb435014819cca97ac
Bjarte bore <bjarte.bore@olavstoppen.no>
2021-12-15 12:43:48 +0100

    Fixed build issue regarding android lifecycle

--- android/src/main/kotlin/com/mapbox/mapboxgl/MapboxMapsPlugin.kt
+++ android/src/main/kotlin/com/mapbox/mapboxgl/MapboxMapsPlugin.kt
@@ -3,20 +3,14 @@
 // found in the LICENSE file.
 package com.mapbox.mapboxgl
 
-import android.app.Activity
-import android.app.Application
-import android.os.Bundle
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LifecycleRegistry
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterAssets
 import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-import io.flutter.embedding.engine.plugins.lifecycle.HiddenLifecycleReference
+import io.flutter.embedding.engine.plugins.lifecycle.FlutterLifecycleAdapter
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /**
  * Plugin for controlling a set of MapboxMap views to be shown as overlays on top of the Flutter
@@ -62,118 +56,13 @@ class MapboxMapsPlugin : FlutterPlugin, ActivityAware {
         lifecycle = null
     }
 
-    private class ProxyLifecycleProvider(activity: Activity) : Application.ActivityLifecycleCallbacks, LifecycleOwner, LifecycleProvider {
-        private val lifecycle = LifecycleRegistry(this)
-        private val registrarActivityHashCode: Int
-        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-            if (activity.hashCode() != registrarActivityHashCode) {
-                return
-            }
-            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
-        }
-
-
-        override fun onActivityStarted(activity: Activity) {
-            if (activity.hashCode() != registrarActivityHashCode) {
-                return
-            }
-            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_START)
-        }
-
-        override fun onActivityResumed(activity: Activity) {
-            if (activity.hashCode() != registrarActivityHashCode) {
-                return
-            }
-            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
-        }
-
-        override fun onActivityPaused(activity: Activity) {
-            if (activity.hashCode() != registrarActivityHashCode) {
-                return
-            }
-            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
-        }
-
-        override fun onActivityStopped(activity: Activity) {
-            if (activity.hashCode() != registrarActivityHashCode) {
-                return
-            }
-            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
-        }
-
-        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
-        override fun onActivityDestroyed(activity: Activity) {
-            if (activity.hashCode() != registrarActivityHashCode) {
-                return
-            }
-            activity.application.unregisterActivityLifecycleCallbacks(this)
-            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-        }
-
-        override fun getLifecycle(): Lifecycle {
-            return lifecycle
-        }
-
-        init {
-            registrarActivityHashCode = activity.hashCode()
-            activity.application.registerActivityLifecycleCallbacks(this)
-        }
-    }
-
     interface LifecycleProvider {
         fun getLifecycle(): Lifecycle?
-    }
-
-    /** Provides a static method for extracting lifecycle objects from Flutter plugin bindings.  */
-    object FlutterLifecycleAdapter {
-        /**
-         * Returns the lifecycle object for the activity a plugin is bound to.
-         *
-         *
-         * Returns null if the Flutter engine version does not include the lifecycle extraction code.
-         * (this probably means the Flutter engine version is too old).
-         */
-        fun getActivityLifecycle(
-                activityPluginBinding: ActivityPluginBinding): Lifecycle {
-            val reference = activityPluginBinding.lifecycle as HiddenLifecycleReference
-            return reference.lifecycle
-        }
     }
 
     companion object {
         private const val VIEW_TYPE = "plugins.flutter.io/mapbox_gl"
         var flutterAssets: FlutterAssets? = null
 
-/*
-        fun registerWith(registrar: Registrar) {
-            val activity = registrar.activity()
-                    ?: // When a background flutter view tries to register the plugin, the registrar has no activity.
-                    // We stop the registration process as this plugin is foreground only.
-                    return
-            if (activity is LifecycleOwner) {
-                registrar
-                        .platformViewRegistry()
-                        .registerViewFactory(
-                                VIEW_TYPE,
-                                MapboxMapFactory(
-                                        registrar.messenger(),
-                                        object : LifecycleProvider {
-                                            override fun getLifecycle(): Lifecycle? {
-                                                return (activity as LifecycleOwner).lifecycle
-                                            }
-                                        }))
-            } else {
-                registrar
-                        .platformViewRegistry()
-                        .registerViewFactory(
-                                VIEW_TYPE,
-                                MapboxMapFactory(registrar.messenger(), ProxyLifecycleProvider(activity)))
-            }
-            val methodChannel = MethodChannel(
-                    registrar.messenger(),
-                    "plugins.flutter.io/mapbox_gl"
-            )
-            methodChannel.setMethodCallHandler(GlobalMethodHandler(registrar))
-        }*/
     }
 }
